241e7b8f462d927b3e2651f42e999eaa63795027
muhamad gatot <gatotbima2002@gmail.com>
2025-06-28 20:07:57 +0700

    Initial Commit

---
+++ KOKIKU/KOKIKUApp.swift
@@ -0,0 +1,17 @@
+//
+//  KOKIKUApp.swift
+//  KOKIKU
+//
+//  Created by Muhamad Gatot Supiadin on 28/06/25.
+//
+
+import SwiftUI
+
+@main
+struct KOKIKUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
